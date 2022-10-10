413ec3d7e91fbd1ca73587898ccbd609e251ffba
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-10-10 13:31:00 +0700

    Tailwindcss Config

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div class="min-h-screen">Homepage</div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter'],
+      },
+      colors: {
+        primary: {
+          DEFAULT: colors.orange[500],
+          ...colors.orange
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@headlessui/tailwindcss')
+  ],
 }
