3bd426eab7b8f89f9300e24098092aea13507e1b
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-10-10 13:20:14 +0700

    nuxtjs config & generated tailwindcss config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt'],
+  tailwindcss: {
+    cssPath: '~/assets/main.css'
+  },
   runtimeConfig: {
     MONGO_URI: process.env.MONGO_URI
+  },
+  nitro: {
+    plugins: ['@/server/db/index.ts']
+  },
+  build: {
+    transpile: [
+      '@headlessui/vue',
+      'vue-toastification',
+      '@headlessui/tailwindcss'
+    ]
   }
 })
